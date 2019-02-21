c6ab2e9a251b90adc765f7eeed97701cf4c9ff73
aesclone <43333330+aesclone@users.noreply.github.com>
2019-02-20 19:42:46 -0600

    Completed the estimated research and development cost estimates

---
+++ Manufacturing_Costs.py
@@ -0,0 +1,12 @@
+# Andrew Scherping
+# 2/14/2019
+# Script to determine the manufacturing costs of an aircraft are the profit produced
+
+N_program = 1000  # Number of aircraft produced during the program
+N_aquired = 50  # The number of aircraft acquired by a company
+C_rdte = 0  # Cost of the research, development, testing and evaluation stage
+
+# N_program = N_m + N_rdte
+# C_acq = C_man + C_pro
+
+# Aircraft_price = (C_man + C_pro + C_rdte) / N_m  # Unit price for an aircraft
